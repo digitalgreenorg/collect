53e88f358ad924e6957bf4d682488e60854f4a76
Hélène Martin <lognaturel@gmail.com>
2021-04-12 14:29:04 -0700

    Remove forum link

--- collect_app/src/main/java/org/odk/collect/android/activities/AboutActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/AboutActivity.java
@@ -45,12 +45,9 @@ public class AboutActivity extends CollectAbstractActivity implements
     private static final String LICENSES_HTML_PATH = "file:///android_asset/open_source_licenses.html";
     private static final String GOOGLE_PLAY_URL = "https://play.google.com/store/apps/details?id=";
     private static final String ODK_WEBSITE = "https://getodk.org";
-    private static final String ODK_FORUM = "https://forum.getodk.org";
 
     private CustomTabHelper websiteTabHelper;
-    private CustomTabHelper forumTabHelper;
     private Uri websiteUri;
-    private Uri forumUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +57,6 @@ public class AboutActivity extends CollectAbstractActivity implements
 
         int[][] items = {
                 {R.drawable.ic_website, R.string.odk_website, R.string.odk_website_summary},
-                {R.drawable.ic_forum, R.string.odk_forum, R.string.odk_forum_summary},
                 {R.drawable.ic_share, R.string.tell_your_friends, R.string.tell_your_friends_msg},
                 {R.drawable.ic_review_rate, R.string.leave_a_review, R.string.leave_a_review_msg},
                 {R.drawable.ic_stars, R.string.all_open_source_licenses, R.string.all_open_source_licenses_msg}
@@ -73,10 +69,8 @@ public class AboutActivity extends CollectAbstractActivity implements
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         websiteTabHelper = new CustomTabHelper();
-        forumTabHelper = new CustomTabHelper();
 
         websiteUri = Uri.parse(ODK_WEBSITE);
-        forumUri = Uri.parse(ODK_FORUM);
     }
 
     private void initToolbar() {
@@ -93,9 +87,6 @@ public class AboutActivity extends CollectAbstractActivity implements
                     websiteTabHelper.openUri(this, websiteUri);
                     break;
                 case 1:
-                    forumTabHelper.openUri(this, forumUri);
-                    break;
-                case 2:
                     Intent shareIntent = new Intent(Intent.ACTION_SEND);
                     shareIntent.setType("text/plain");
                     shareIntent.putExtra(Intent.EXTRA_TEXT,
@@ -104,7 +95,7 @@ public class AboutActivity extends CollectAbstractActivity implements
                     startActivity(Intent.createChooser(shareIntent,
                             getString(R.string.tell_your_friends)));
                     break;
-                case 3:
+                case 2:
                     boolean intentStarted = false;
                     try {
                         // Open the google play store app if present
@@ -134,7 +125,7 @@ public class AboutActivity extends CollectAbstractActivity implements
                                 Uri.parse(GOOGLE_PLAY_URL + getPackageName())));
                     }
                     break;
-                case 4:
+                case 3:
                     Intent intent = new Intent(this, WebViewActivity.class);
                     intent.putExtra(CustomTabHelper.OPEN_URL, LICENSES_HTML_PATH);
                     startActivity(intent);
@@ -147,13 +138,11 @@ public class AboutActivity extends CollectAbstractActivity implements
     public void onStart() {
         super.onStart();
         websiteTabHelper.bindCustomTabsService(this, websiteUri);
-        forumTabHelper.bindCustomTabsService(this, forumUri);
     }
 
     @Override
     public void onDestroy() {
         unbindService(websiteTabHelper.getServiceConnection());
-        unbindService(forumTabHelper.getServiceConnection());
         super.onDestroy();
     }
 }
